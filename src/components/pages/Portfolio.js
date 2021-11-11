18a31e68dda8ea26ba11834b6bd00650bd09eb4b
Cameron H <cameronheadlee6@gmail.com>
2021-11-11 16:35:29 -0500

    made nav button larger and adjusted spacing.

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -11,7 +11,7 @@ function Portfolio(props) {
           <h1 className='d-flex justify-content-center pt-2'>Portfolio</h1>
             <p className='description'>Below you will find a few projects I have created solo and with a team. Click on the <FontAwesomeIcon icon={['fab', 'github']} size='lg'/> next to the project name to view the GitHub repository and code. If you click on the <FontAwesomeIcon icon='eye' size='lg' /> and button, you will view the deployed application!</p>
               {props.children}
-                <div className='d-flex justify-content-end py-5 px-5'>
+                <div className='d-flex justify-content-end'>
                   <Link to="/Contact">
                     <Button className="contact" variant='outline-three'>
                       Contact
