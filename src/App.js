2361deb1c30afdecf7606d50ba8761c87002de90
Cameron H <cameronheadlee6@gmail.com>
2021-11-11 00:00:36 -0500

    Added the icons I am using in fontawesome.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,14 @@
 import React from 'react';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { 
+  faLongArrowAltRight, 
+  faLongArrowAltLeft, 
+  faPaperPlane, 
+  faEye,
+  faFileDownload,
+  faUndoAlt
+} from '@fortawesome/free-solid-svg-icons';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Project from './components/Project';
@@ -10,6 +20,14 @@ import Portfolio from './components/pages/Portfolio';
 import Resume from './components/pages/Resume';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
+library.add(
+  fab, 
+  faLongArrowAltRight, 
+  faLongArrowAltLeft, 
+  faPaperPlane, 
+  faEye, 
+  faFileDownload,
+  faUndoAlt);
 
 function App() {
   return (
