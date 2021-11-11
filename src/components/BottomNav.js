efd9c44e80b8514f0bfd0aad089bb7ffc0deedcc
Cameron H <cameronheadlee6@gmail.com>
2021-11-11 16:33:15 -0500

    Took out inline styling for its own css file.

--- src/components/BottomNav.js
+++ src/components/BottomNav.js
@@ -5,45 +5,28 @@ import Nav from 'react-bootstrap/Nav';
 import '../styles/BottomNav.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-const styles = {
-  navStyles: {
-    backgroundColor: '#9a1aef'
-  }
-}
-
 function BottomNav() {
     return (
         <>
-          <Navbar  style={styles.navStyles}  collapseOnSelect className='justify-content-center' sticky='bottom'>
-        <Nav>
-           <Link to={{pathname: "https://www.linkedin.com/in/cameron-headlee"}} target="_blank"
-            
-            > 
-            <div className='text-light'>
-              <FontAwesomeIcon className='bottom' icon={[ 'fab', 'linkedin' ]} size='3x' fixedWidth />    
-            </div>
-          
-          </Link> 
-            <Link to={{pathname: "https://twitter.com/FullStackCam"}} target="_blank"
-          
-          >
-            <div className='text-light'>
-            <FontAwesomeIcon className='bottom' icon={['fab', 'twitter']} size='3x' fixedWidth/>
-            </div>
-          
-          </Link>
-          
-         <Link to={{pathname: "https://github.com/CameronHeadlee"}} target="_blank"
-          
-          >
-            <div className='text-light hover'>
-            <FontAwesomeIcon className='bottom' icon={['fab', 'github']} size='3x' fixedWidth />
-            </div>
-           
-          
-        </Link>
-        </Nav>
-       </Navbar>
+          <Navbar collapseOnSelect className='nav justify-content-center' sticky='bottom'>
+            <Nav>
+              <Link to={{pathname: "https://www.linkedin.com/in/cameron-headlee"}} target="_blank"> 
+                <div className='text-light'>
+                  <FontAwesomeIcon className='bottom' icon={[ 'fab', 'linkedin' ]} size='2x' fixedWidth />    
+                </div>
+              </Link> 
+              <Link to={{pathname: "https://twitter.com/FullStackCam"}} target="_blank">
+                <div className='text-light'>
+                  <FontAwesomeIcon className='bottom' icon={['fab', 'twitter']} size='2x' fixedWidth/>
+                </div>
+              </Link>
+              <Link to={{pathname: "https://github.com/CameronHeadlee"}} target="_blank">
+                <div className='text-light hover'>
+                  <FontAwesomeIcon className='bottom' icon={['fab', 'github']} size='2x' fixedWidth />
+                </div>
+              </Link>
+            </Nav>
+          </Navbar>
       </>
     );
 }
