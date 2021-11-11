f30caaf42f049448bd57b2170f2d20b88819b132
Cameron H <cameronheadlee6@gmail.com>
2021-11-11 16:33:32 -0500

    Took out inline styling for its own css file.

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,54 +3,37 @@ import { Link } from 'react-router-dom';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Button from 'react-bootstrap/Button';
-
-const styles = {
-    navStyles: {
-    backgroundColor: '#9a1aef',
-    },
-    navHeader: {
-        color: '#f1faee'
-    }
-}
+import '../styles/Navigation.css';
 
 function Navigation() {
     return (
         <>
-    <Navbar style={styles.navStyles} sticky='top' collapseOnSelect expand="lg" >
-        <Navbar.Brand><h1 style={styles.navHeader}>Cameron Headlee</h1></Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse className='justify-content-end' id="responsive-navbar-nav">
-        <Nav.Link>
-            <Link to="/R-Portfolio"
-                   
-               ><Button variant="outline-light">About</Button>
-
-            </Link>  
-        </Nav.Link>
-        <Nav.Link>
-            <Link to="/Portfolio"
-                     
-                ><Button variant="outline-light">My Work</Button>
-
-            </Link>
-        </Nav.Link>
-        <Nav.Link>
-            <Link to="/Contact"
-                    
-                    
-                > <Button variant="outline-light">Contact</Button>
-
-            </Link>
-        </Nav.Link>
-        <Nav.Link>
-            <Link to="/Resume"
-
-                ><Button variant="outline-light">Resume</Button>
-
-            </Link>
-        </Nav.Link>
-        </Navbar.Collapse>
-    </Navbar>
+    <Navbar className='nav' sticky='top' collapseOnSelect expand="lg">
+        <Navbar.Brand className='ps-5'><h1 className='top'>Cameron Headlee</h1></Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse className='justify-content-end' id="responsive-navbar-nav">
+                    <Nav.Link>
+                        <Link to="/R-Portfolio">   
+                            <Button variant="outline-light">About</Button>
+                        </Link>  
+                    </Nav.Link>
+                    <Nav.Link>
+                        <Link  to="/Portfolio">   
+                            <Button variant="outline-light">My Work</Button>
+                        </Link>
+                    </Nav.Link>
+                    <Nav.Link>
+                        <Link to="/Contact">
+                            <Button variant="outline-light">Contact</Button>
+                        </Link>
+                    </Nav.Link>
+                    <Nav.Link>
+                        <Link to="/Resume">
+                            <Button variant="outline-light">Resume</Button>
+                        </Link>
+                    </Nav.Link>
+                </Navbar.Collapse>
+        </Navbar>
       </> 
     );
 }
