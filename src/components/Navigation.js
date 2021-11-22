6f7058baaec30317e089293ff980c494e4eb951c
Cameron H <cameronheadlee6@gmail.com>
2021-11-22 15:12:20 -0500

    Changed single quotes to double quotes and adjusted formatting of code.

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,40 +1,40 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import Button from 'react-bootstrap/Button';
-import '../styles/Navigation.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Button from "react-bootstrap/Button";
+import "../styles/Navigation.css";
 
 function Navigation() {
     return (
-        <>
-    <Navbar className='nav' sticky='top' collapseOnSelect expand="lg">
-        <Navbar.Brand className='text-light fs-2 ps-2'>Cameron Headlee</Navbar.Brand>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse className='justify-content-end' id="responsive-navbar-nav">
-                    <Nav.Link>
-                        <Link to="/R-Portfolio">   
-                            <Button variant="outline-light">About</Button>
-                        </Link>  
-                    </Nav.Link>
-                    <Nav.Link>
-                        <Link  to="/Portfolio">   
-                            <Button variant="outline-light">My Work</Button>
-                        </Link>
-                    </Nav.Link>
-                    <Nav.Link>
-                        <Link to="/Contact">
-                            <Button variant="outline-light">Contact</Button>
-                        </Link>
-                    </Nav.Link>
-                    <Nav.Link>
-                        <Link to="/Resume">
-                            <Button variant="outline-light">Resume</Button>
-                        </Link>
-                    </Nav.Link>
+    <>
+        <Navbar className="nav" sticky="top" collapseOnSelect expand="lg">
+            <Navbar.Brand className="text-light fs-2 ps-2">Cameron Headlee</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse className="justify-content-end" id="responsive-navbar-nav">
+                        <Nav.Link>
+                            <Link to="/R-Portfolio">   
+                                <Button variant="outline-light">About</Button>
+                            </Link>  
+                        </Nav.Link>
+                        <Nav.Link>
+                            <Link  to="/Portfolio">   
+                                <Button variant="outline-light">My Work</Button>
+                            </Link>
+                        </Nav.Link>
+                        <Nav.Link>
+                            <Link to="/Contact">
+                                <Button variant="outline-light">Contact</Button>
+                            </Link>
+                        </Nav.Link>
+                        <Nav.Link>
+                            <Link to="/Resume">
+                                <Button variant="outline-light">Resume</Button>
+                            </Link>
+                        </Nav.Link>
                 </Navbar.Collapse>
         </Navbar>
-      </> 
+    </> 
     );
 }
 
