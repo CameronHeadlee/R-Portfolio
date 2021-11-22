10e0fae086907305b89123973acedeb1c7575050
Cameron H <cameronheadlee6@gmail.com>
2021-11-22 15:12:35 -0500

    Changed single quotes to double quotes and adjusted formatting of code.

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,10 +1,10 @@
-import React, { useRef } from 'react';
-import emailjs from 'emailjs-com';
-import { Link } from 'react-router-dom';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import '../../styles/contact.css';
+import React, { useRef } from "react";
+import emailjs from "emailjs-com";
+import { Link } from "react-router-dom";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "../../styles/contact.css";
 
 const Contact = () =>  {
     const form = useRef();
@@ -13,10 +13,10 @@ const Contact = () =>  {
         e.preventDefault();
 
         emailjs.sendForm(
-        'service_v1bo3ik',
-         'template_aja9t0y',
+        "service_v1bo3ik",
+         "template_aja9t0y",
          e.target, 
-        'user_y6IkqpSNXsUDGBmTIEhDw'
+        "user_y6IkqpSNXsUDGBmTIEhDw"
         ).then(res => {
             console.log(res);
         }).catch(err => console.log(err));
@@ -24,68 +24,66 @@ const Contact = () =>  {
     }
     return (
     <>
-        <div className='main'>
-            <div className='container'>
-            <div className='row'>
-                <div className='col d-flex justify-content-center'>
-                <h1>Contact</h1>
-                </div>
-            </div>
-            <div className='row'>
-                <div className='col d-flex justify-content-center'>
-                <p className='below'>Below you will find the best phone number to reach me at as well as a simple contact form to send me an email. Use either to contact me at any point. If you would like to email me, simply fill out the form with the stated information and I will respond as soon as possible!</p>
-                </div>
-            </div>
-            <div className='row'>
-                <div className='col d-flex justify-content-center'>
-                <h1>Phone:</h1>
-                </div>
-            </div>
-            <div className='row'>
-                <div className='col d-flex justify-content-center'>
-                <p className='cell'>(614)-517-7587</p>
+        <div className="main">
+            <div className="container">
+                <div className="row">
+                    <div className="col d-flex justify-content-center">
+                    <h1>Contact</h1>
                     </div>
                 </div>
-            
-                <Form className='pt-3' ref={form} onSubmit={sendEmail}>
-                <div className='row'>
-                    <div className='col d-flex justify-content-center'>
+                <div className="row">
+                    <div className="col d-flex justify-content-center">
+                        <p className="below">Below you will find the best phone number to reach me at as well as a simple contact form to send me an email. Use either to contact me at any point. If you would like to email me, simply fill out the form with the stated information and I will respond as soon as possible!</p>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col d-flex justify-content-center">
+                        <h1>Phone:</h1>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col d-flex justify-content-center">
+                        <p className="cell">(614)-517-7587</p>
+                    </div>
+                </div>
+            <Form className="pt-3" ref={form} onSubmit={sendEmail}>
+                <div className="row">
+                    <div className="col d-flex justify-content-center">
                         <Form.Group controlId="exampleForm.ControlInput1">
-                            <Form.Label className='phone d-flex justify-content-center'>Email</Form.Label>
+                            <Form.Label className="phone d-flex justify-content-center">Email</Form.Label>
                                 <Form.Control type="email" name="user_email" />
                         </Form.Group>
-                        </div>
-                    </div>    
-                    <div className='row'>
-                    <div className='col'>
+                    </div>
+                </div>    
+                <div className="row">
+                    <div className="col">
                         <Form.Group controlId="exampleForm.ControlTextarea1">
-                            <Form.Label className='phone d-flex justify-content-center'>Message</Form.Label>
+                            <Form.Label className="phone d-flex justify-content-center">Message</Form.Label>
                                 <Form.Control as="textarea" name="message" rows={5}/>
                         </Form.Group>
-                        </div>
                     </div>
-                    <div className='row'>
-                    <div className='col d-flex justify-content-center'>
+                </div>
+                <div className="row">
+                    <div className="col d-flex justify-content-center">
                         <Button variant="outline-mail" type="submit">
                             Send
                             <FontAwesomeIcon icon="paper-plane" />
                         </Button>
-                        </div>
                     </div>
-                </Form>
-              
-                <div className='row'>
-                    <div className='col d-flex justify-content-end'>
-                    <Link to="/Resume">
-                        <Button variant='outline-two'>
-                            Resume
-                            <FontAwesomeIcon icon="long-arrow-alt-right" size="lg" fixedWidth />
-                        </Button>
-                    </Link>
+                </div>
+            </Form>
+                <div className="row">
+                    <div className="col d-flex justify-content-end">
+                        <Link to="/Resume">
+                            <Button variant="outline-two">
+                                Resume
+                                <FontAwesomeIcon icon="long-arrow-alt-right" size="lg" fixedWidth />
+                            </Button>
+                        </Link>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
     </>
     );
 }
