69aacaab4549e63a6063229799d737344e0560cd
Cameron H <cameronheadlee6@gmail.com>
2022-01-09 00:33:53 -0500

    Fixed spacing and responsiveness on contact page.

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -32,20 +32,24 @@ const Contact = () =>  {
                         <p className="below">You can use any of the methods below to contact me!</p>
                     </div>
                 </div>
-                <div className="row">
-                    <div className="col d-flex justify-content-center">
-                        <ListGroup horizontal>
-                            <ListGroup.Item>Cellphone</ListGroup.Item>
-                            <ListGroup.Item>Link to Email</ListGroup.Item>
-                            <ListGroup.Item>Email form</ListGroup.Item>
-                        </ListGroup>
+                <div className="row d-flex justify-content-around">
+                    <div className="col-md-auto">
+                        <p className="method">Cellphone</p>
+                    </div>
+                    <div className="col-md-auto">
+                        <p className="method">Email Form</p>
+                    </div>
+                    <div className="col-md-auto">
+                        <p className="method">Link to Email</p>
                     </div>
                 </div>
-                <div className="row">
-                    <div className="col d-flex justify-content-between">
-                        <h1 className="link">Phone: (614)-517-7587</h1>
-                        <a target="_blank" rel="noreferrer" href = "mailto: camhcodes11@gmail.com">
-                            <h1 className="link">Email Link</h1>
+                <div className="row d-flex justify-content-between">
+                    <div className="col-md-auto">
+                        <p className="cell">(614)-517-7587</p>
+                    </div>
+                    <div className="col-md-auto">
+                        <a target="_blank" rel="noreferrer" href="mailto: camhcodes11@gmail.com">
+                            <p className="link">Email Link</p>
                         </a>
                     </div>
                 </div>
@@ -54,7 +58,7 @@ const Contact = () =>  {
                     <div className="col d-flex justify-content-center">
                         <Form.Group controlId="exampleForm.ControlInput1">
                             <Form.Label className="phone d-flex justify-content-center">Email</Form.Label>
-                                <Form.Control type="email" name="user_email" />
+                            <Form.Control type="email" name="user_email"/>
                         </Form.Group>
                     </div>
                 </div>    
@@ -62,7 +66,7 @@ const Contact = () =>  {
                     <div className="col">
                         <Form.Group controlId="exampleForm.ControlTextarea1">
                             <Form.Label className="phone d-flex justify-content-center">Message</Form.Label>
-                                <Form.Control as="textarea" name="message" rows={5}/>
+                            <Form.Control as="textarea" name="message" rows={5}/>
                         </Form.Group>
                     </div>
                 </div>
@@ -70,7 +74,7 @@ const Contact = () =>  {
                     <div className="col d-flex justify-content-center">
                         <Button variant="outline-mail" type="submit">
                             Send
-                            <FontAwesomeIcon icon="paper-plane" />
+                            <FontAwesomeIcon icon="paper-plane"/>
                         </Button>
                     </div>
                 </div>
