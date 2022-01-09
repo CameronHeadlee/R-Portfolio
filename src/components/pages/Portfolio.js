b19390e3a03ee16a4654584c95b8596c61e7d60c
Cameron H <cameronheadlee6@gmail.com>
2022-01-08 22:43:55 -0500

    Added back button and changed page directions to take up less space.

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -11,17 +11,28 @@ function Portfolio(props) {
         <div className="container">
           <div className="row">
             <div className="col d-flex justify-content-center">
-              <h1>Portfolio</h1>
+            <p className="below">Below are a few projects I have created solo and with a team!</p>
             </div>
           </div>
           <div className="row">
             <div className="col d-flex justify-content-center">
-              <p className="description">Below you will find a few projects I have created solo and with a team. Click on the <FontAwesomeIcon icon={["fab", "github"]} size="lg"/> next to the project name to view the GitHub repository and code. If you click on the <FontAwesomeIcon icon="eye" size="lg" /> and button, you will view the deployed application!</p>
+              <ul className="description">
+                <li>Click on the <FontAwesomeIcon icon={["fab", "github"]} size="lg"/> next to the project name to view the GitHub repository and code.</li> 
+                <li>Click on the <FontAwesomeIcon icon="eye" size="lg" /> and button, you can view the deployed application.</li>
+              </ul>
             </div>
           </div>
             {props.children}
-          <div className="row">
-            <div className="col d-flex justify-content-end">
+          <div className="row d-flex justify-content-between">
+          <div className="col-md-auto">
+              <Link to="/About">
+                <Button className="contact" variant="outline-eight">
+                  <FontAwesomeIcon icon="long-arrow-alt-left" size="lg" fixedWidth />
+                  Back
+                </Button>
+              </Link>
+            </div>
+            <div className="col-md-auto">
               <Link to="/Contact">
                 <Button className="contact" variant="outline-three">
                   Contact
