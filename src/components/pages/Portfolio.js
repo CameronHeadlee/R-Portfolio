d6c19fb51f68e3761a7df94e3197cb5397d5286f
Cameron H <cameronheadlee6@gmail.com>
2022-02-17 19:27:32 -0500

    Fixed code format for readability.

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -7,42 +7,48 @@ import "../../styles/Portfolio.css";
 function Portfolio(props) {
     return (
     <>
-      <div className="main">
-        <div className="container">
+    <div className="main">
+      <div className="container">
           <div className="row">
-            <div className="col d-flex justify-content-center">
-            <p className="below">Below are a few projects I have created solo and with a team!</p>
-            </div>
+          <div className="col d-flex justify-content-center">
+            <p className="below">
+              Below are a few projects I have created solo and with a team!
+            </p>
+          </div>
           </div>
           <div className="row">
-            <div className="col d-flex justify-content-center">
-              <ul className="description">
-                <li>Click on the <FontAwesomeIcon icon={["fab", "github"]} size="lg"/> next to the project name to view the GitHub repository and code.</li> 
-                <li>Click on the <FontAwesomeIcon icon="eye" size="lg" /> and button, you can view the deployed application.</li>
-              </ul>
-            </div>
+          <div className="col d-flex justify-content-center">
+            <ul className="description">
+              <li>
+                Click on the <FontAwesomeIcon icon={["fab", "github"]} size="lg"/> next to the project name to view the GitHub repository and code.
+              </li> 
+              <li>
+                Click on the <FontAwesomeIcon icon="eye" size="lg" /> at the bottom of each project to view the deployed application.
+              </li>
+            </ul>
+          </div>
           </div>
             {props.children}
           <div className="row d-flex justify-content-between">
           <div className="col-md-auto">
-              <Link to="/About">
-                <Button className="contact" variant="outline-eight">
-                  <FontAwesomeIcon icon="long-arrow-alt-left" size="lg" fixedWidth />
-                  Back
-                </Button>
-              </Link>
-            </div>
-            <div className="col-md-auto">
-              <Link to="/Contact">
-                <Button className="contact" variant="outline-three">
-                  Contact
-                  <FontAwesomeIcon icon="long-arrow-alt-right" size="lg" fixedWidth />
-                </Button>
-              </Link>
-            </div>
+            <Link to="/About">
+              <Button className="contact" variant="outline-eight">
+                <FontAwesomeIcon icon="long-arrow-alt-left" size="lg" fixedWidth />
+                Back
+              </Button>
+            </Link>
+          </div>
+          <div className="col-md-auto">
+            <Link to="/Contact">
+              <Button className="contact" variant="outline-three">
+                Contact
+                <FontAwesomeIcon icon="long-arrow-alt-right" size="lg" fixedWidth />
+              </Button>
+            </Link>
           </div>
         </div>
       </div>
+    </div>
     </>
     );
 }
