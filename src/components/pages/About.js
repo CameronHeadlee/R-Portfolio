0a3c0dc3373b1b7f601831c50794e27f4d9a2dbf
Cameron H <cameronheadlee6@gmail.com>
2022-02-17 19:26:31 -0500

    Changed about me paragraph. Changed color scheme.

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -12,61 +12,65 @@ function About() {
    
     return (
     <>
-        <div className="main">
-            <div className="container">
-                <div className="row">
-                    <div className="col">
-                        <Accordion>
-                            <Accordion.Item eventKey="0"> 
-                                <Accordion.Header><h2>About me</h2></Accordion.Header>
-                                    <Accordion.Body className="me"> 
-                                        I am a Full Stack Web developer with a certificate in Full Stack Development from The Ohio State University, combined with previous managerial and lead sales associate experience. Front End development, database theory, APIs, and git incorporated with my knowledge of the MERN stack allow me to collaborate on Full Stack applications with a team such as JoinMe. My previous work experience as a sales associate has left me with great communication and interpersonal skills. My role as a manager has provided me with the ability to stay organized as well as motivate others. Development has created a special place in my heart because it inspires me to create original web applications daily. My passion for coding combined with my previous work ventures is why web development is the right career for me. 
-                                    </Accordion.Body>
-                            </Accordion.Item>
-                        </Accordion>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col">
-                        <Accordion className="second">
-                            <Accordion.Item eventKey="1">
-                                <Accordion.Header><h2>Favorite Quote</h2></Accordion.Header>
-                                    <Accordion.Body className="me">
-                                        "No man ever steps in the same river twice, for it"s not the same river and he"s not the same man." 
-                                        - Heraclitus
-                                    </Accordion.Body>
-                            </Accordion.Item>
-                        </Accordion>
-                    </div>
-                </div>
-                <div className="row d-flex justify-content-around">
-                    <div className="col-md-auto">
-                        <Image className="picture" src ={Img} alt="Me jumping on a snowboard."/>
-                    </div>
-                    <div className="col-md-auto">
-                        <Image className="picture" src ={Img2} alt="Lacrosse goal celebration with team."/>
-                    </div>
-                </div>
-                <div className="row d-flex justify-content-between">
-                <div className="col-md-auto">
-                        <Link to="/R-Portfolio">
-                            <Button variant="outline-seven">
-                                <FontAwesomeIcon icon="long-arrow-alt-left" size="lg" fixedWidth />
-                                Back
-                            </Button>
-                        </Link>
-                    </div>
-                    <div className="col-md-auto">
-                        <Link to="/Portfolio">
-                            <Button variant="outline-one">
-                                My Work
-                                <FontAwesomeIcon icon="long-arrow-alt-right" size="lg" fixedWidth />
-                            </Button>
-                        </Link>
-                    </div>
-                </div>
+    <div className="main">
+        <div className="container">
+            <div className="row">
+            <div className="col">
+                <Accordion>
+                    <Accordion.Item eventKey="0"> 
+                        <Accordion.Header>
+                            <h2>About me</h2>
+                        </Accordion.Header>
+                        <Accordion.Body className="me"> 
+                            I am a Full Stack Web Developer with a certificate in Full Stack Development from The Ohio State University, combined with previous managerial and lead sales associate experience. Finding a career that I could enjoy every day of my life and challenge me along the way has always been my goal. I struggled for a while to find anything that could meet the small criteria I had for a career. After working in a few positions to gain experience and explore my passions, I finally found coding through my brother. He had recently graduated a Bootcamp himself and found a position at an awesome company with the opportunity for lots of vertical growth. From seeing his success and happiness, I began looking deeper into coding and finally realized I had found what I had been looking for. My passion for coding,  previous work ventures, and my love for challenging myself is why web development is the right career for me.
+                        </Accordion.Body>
+                    </Accordion.Item>
+                </Accordion>
+            </div>
+            </div>
+            <div className="row">
+            <div className="col">
+                <Accordion className="second">
+                    <Accordion.Item eventKey="1">
+                        <Accordion.Header>
+                        <h2>Favorite Quote</h2>
+                        </Accordion.Header>
+                        <Accordion.Body className="me">
+                            "No man ever steps in the same river twice, for it"s not the same river and he"s not the same man." 
+                            - Heraclitus
+                        </Accordion.Body>
+                    </Accordion.Item>
+                </Accordion>
+            </div>
+            </div>
+            <div className="row d-flex justify-content-between">
+            <div className="col-md-auto">
+                <Image className="picture" src ={Img} alt="Me jumping on a snowboard."/>
+            </div>
+            <div className="col-md-auto">
+                <Image className="picture" src ={Img2} alt="Lacrosse goal celebration with team."/>
+            </div>
+            </div>
+            <div className="row d-flex justify-content-between">
+            <div className="col-md-auto">
+                <Link to="/R-Portfolio">
+                    <Button variant="outline-seven">
+                    <FontAwesomeIcon icon="long-arrow-alt-left" size="lg" fixedWidth />
+                        Back
+                    </Button>
+                </Link>
+            </div>
+            <div className="col-md-auto">
+                <Link to="/Portfolio">
+                    <Button variant="outline-one">
+                        My Work
+                    <FontAwesomeIcon icon="long-arrow-alt-right" size="lg" fixedWidth />
+                    </Button>
+                </Link>
+            </div>
             </div>
         </div>
+    </div>
     </>
     );
 }
