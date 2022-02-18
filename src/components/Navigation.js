49c90cd7e172d2ca37eabb3d1aa3a7d64a25054d
Cameron H <cameronheadlee6@gmail.com>
2022-02-17 19:25:28 -0500

    Changed color scheme of header nav and styking of page buttons. Added link to name to return user to home page.

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -8,37 +8,41 @@ import "../styles/Navigation.css";
 function Navigation() {
     return (
     <>
-        <Navbar className="nav" sticky="top" collapseOnSelect expand="lg">
-            <Navbar.Brand className="text-light fs-2 ps-2">Cameron Headlee</Navbar.Brand>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                    <Navbar.Collapse className="justify-content-end" id="responsive-navbar-nav">
-                    <Nav.Link>
-                            <Link to="/R-Portfolio">   
-                                <Button variant="outline-light">Home</Button>
-                            </Link>  
-                        </Nav.Link>
-                        <Nav.Link>
-                            <Link to="/About">   
-                                <Button variant="outline-light">About</Button>
-                            </Link>  
-                        </Nav.Link>
-                        <Nav.Link>
-                            <Link  to="/Portfolio">   
-                                <Button variant="outline-light">My Work</Button>
-                            </Link>
-                        </Nav.Link>
-                        <Nav.Link>
-                            <Link to="/Contact">
-                                <Button variant="outline-light">Contact</Button>
-                            </Link>
-                        </Nav.Link>
-                        <Nav.Link>
-                            <Link to="/Resume">
-                                <Button variant="outline-light">Resume</Button>
-                            </Link>
-                        </Nav.Link>
-                </Navbar.Collapse>
-        </Navbar>
+    <Navbar className="nav" sticky="top" collapseOnSelect expand="lg">
+        <Navbar.Brand className="fs-2 ps-2">
+            <Link to="/R-Portfolio" className="nav-name">
+                Cameron Headlee
+            </Link>
+        </Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse className="justify-content-end" id="responsive-navbar-nav">
+                <Nav.Link>
+                    <Link to="/R-Portfolio">   
+                        <Button variant="outline-nav">Home</Button>
+                    </Link>  
+                </Nav.Link>
+                <Nav.Link>
+                    <Link to="/About">   
+                        <Button variant="outline-nav">About</Button>
+                    </Link>  
+                </Nav.Link>
+                <Nav.Link>
+                    <Link  to="/Portfolio">   
+                        <Button variant="outline-nav">My Work</Button>
+                    </Link>
+                </Nav.Link>
+                <Nav.Link>
+                    <Link to="/Contact">
+                        <Button variant="outline-nav">Contact</Button>
+                    </Link>
+                </Nav.Link>
+                <Nav.Link>
+                    <Link to="/Resume">
+                        <Button variant="outline-nav">Resume</Button>
+                    </Link>
+                </Nav.Link>
+            </Navbar.Collapse>
+    </Navbar>
     </> 
     );
 }
