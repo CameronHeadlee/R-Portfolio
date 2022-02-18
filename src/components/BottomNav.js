d6592878164515829e4082bdbd28a173b1c48997
Cameron H <cameronheadlee6@gmail.com>
2022-02-17 19:24:44 -0500

    Changed color scheme of footer nav.

--- src/components/BottomNav.js
+++ src/components/BottomNav.js
@@ -8,25 +8,25 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 function BottomNav() {
     return (
     <>
-        <Navbar collapseOnSelect className="nav justify-content-center" sticky="bottom">
-          <Nav>
-            <Link to={{pathname: "https://www.linkedin.com/in/cameron-headlee"}} target="_blank"> 
-              <div className="text-light">
-                <FontAwesomeIcon className="bottom" icon={[ "fab", "linkedin" ]} size="2x" fixedWidth />    
-              </div>
-            </Link> 
-            <Link to={{pathname: "https://twitter.com/FullStackCam"}} target="_blank">
-              <div className="text-light">
-                <FontAwesomeIcon className="bottom" icon={["fab", "twitter"]} size="2x" fixedWidth/>
-              </div>
-            </Link>
-            <Link to={{pathname: "https://github.com/CameronHeadlee"}} target="_blank">
-              <div className="text-light hover">
-                <FontAwesomeIcon className="bottom" icon={["fab", "github"]} size="2x" fixedWidth />
-              </div>
-            </Link>
-          </Nav>
-        </Navbar>
+    <Navbar collapseOnSelect className="nav justify-content-center" sticky="bottom">
+      <Nav>
+        <Link to={{pathname: "https://www.linkedin.com/in/cameron-headlee"}} target="_blank"> 
+          <div className="text-light">
+            <FontAwesomeIcon className="bottom" icon={[ "fab", "linkedin" ]} size="2x" fixedWidth />    
+          </div>
+        </Link> 
+        <Link to={{pathname: "https://twitter.com/FullStackCam"}} target="_blank">
+          <div className="text-light">
+            <FontAwesomeIcon className="bottom" icon={["fab", "twitter"]} size="2x" fixedWidth/>
+          </div>
+        </Link>
+        <Link to={{pathname: "https://github.com/CameronHeadlee"}} target="_blank">
+          <div className="text-light hover">
+            <FontAwesomeIcon className="bottom" icon={["fab", "github"]} size="2x" fixedWidth />
+            </div>
+        </Link>
+      </Nav>
+    </Navbar>
     </>
     );
 }
